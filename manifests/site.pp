6432c70700311c7f3fa2562e4409f9673294c151
Markus Schmall <schmall.markus@gmail.com>
2013-04-21 21:08:44 +0200

    test

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,13 @@
 node flakedummy
 {
-#    include basesetup
-#    include installdb
+    include basesetup
+    include installdb
 
-#    include base
-#    include kippo
+    include base
+    include kippo
 #    include servletpot
 
-#    Class['basesetup'] ->  Class['installdb'] ->  Class['base'] ->  Class['kippo'] ->  Class['servletpot']
+    Class['basesetup'] ->  Class['installdb'] ->  Class['base'] ->  Class['kippo'] ->  Class['ewsposter']
 
 }
 
