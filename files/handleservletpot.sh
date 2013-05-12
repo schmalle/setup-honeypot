2ac11e86deccd5939dbe8ea5b02f20980b46a9f9
Markus Schmall <schmall.markus@gmail.com>
2013-05-12 20:40:04 +0200

    added config scripts for servletpot

--- files/handleservletpot.sh
+++ files/handleservletpot.sh
@@ -5,9 +5,11 @@
 
 if [ -f /data/honeypot/Servletpot/README.txt ];
 then
-cd /data/honeypot/Servletpot; git pull
+  su honeypot -c 'cd /data/honeypot/Servletpot; git pull'
 else
-git clone https://github.com/schmalle/Servletpot.git /data/honeypot/Servletpot
+  su honeypot -c 'git clone https://github.com/schmalle/Servletpot.git /data/honeypot/Servletpot'
+  su honeypot -c 'cd /data/honeypot/Servletpot; ant'
+  rm -rf /var/lib/tomcat7/webapps/ROOT
+  cp /data/honeypot/Servletpot/servletpot.war /var/lib/tomcat7/webapps/ROOT.war
 fi
-
 
