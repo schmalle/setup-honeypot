cea4a0441c9f90bcf484fc1b92f9d1185ad35d75
Markus Schmall <schmall.markus@gmail.com>
2013-06-09 16:01:26 +0200

    small bugfixes

--- modules/basesetup/manifests/init.pp
+++ modules/basesetup/manifests/init.pp
@@ -7,7 +7,6 @@ class basesetup
 #        command => "/bin/sh /data/honeypot/setupcrontab.sh",
 #    }
 
-
  
 
  package {'ant':
@@ -64,19 +63,14 @@ class basesetup
     source => "puppet://$puppetMaster/honeypot_data/logfile"
   }
 
-#  file { '/data/honeypot/crontab.txt':
-#    source => "puppet://$puppetMaster/honeypot_data/crontab.txt"
-#  }
+
 
   file { '/var/spool/cron/crontabs/root':
     source => "puppet://$puppetMaster/honeypot_data/crontab.txt"
   }
 
-#  file { '/data/honeypot/setupcrontab.sh':
-#    source => "puppet://$puppetMaster/honeypot_data/setupcrontab.sh"
-#  }
 
   File['/data/'] -> File['/data/honeypot/'] -> File['/data/honeypot/config/'] -> File['/data/honeypot/readme.txt'] -> File['/data/honeypot/logfile1'] -> File['/data/honeypot/logfile2'] -> File['/data/honeypot/logfile3'] -> File['/var/spool/cron/crontabs/root'] 
-  #-> File['/data/honeypot/setupcrontab.sh'] 
+
 
 }
